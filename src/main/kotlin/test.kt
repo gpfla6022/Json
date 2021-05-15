7b83f8bd42e021a1c5579ee01ae79729c6424b8c
gpfla6022 <gpfla3503@gmail.com>
2021-05-16 02:48:31 +0900

    파일 생성부터 폴더 생성까지

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -1,79 +1,29 @@
 import java.io.File
 
+// 제이슨 데이타 안에 삽입되는 내용은 농장안에 품목등 아무거나 적어도좋음
+
+// "/farmRepository/farm/introduce/data/detail/farmDetail.json"
+
+// 위 경로에 제이슨 생성
+
+
 fun main(){
-
-    zooJson()
-    zooHtml()
+    farmDetail()
 
 }
 
 
-fun zooHtml(){
+fun farmDetail(){
 
-    var testZoo = ""
-
-    testZoo += "<!DOCTYPE html>\r\n"
-    testZoo += "<html> \r\n"
-    testZoo += "<head> \r\n"
-    testZoo += "\t <meta charset = \"UTF-8\"> \r\n"
-    testZoo += "\t <title> 우리 동물원 </title>\r\n"
-    testZoo += "</head> \r\n"
-    testZoo += "<body>\r\n"
-    testZoo += "\t <h1>우리 동물원</h1>\r\n"
-    testZoo += "\t <div>사파리</div>\r\n"
-    testZoo += "\t <div>아쿠아리움</div>\r\n"
-    testZoo += "\t <div>아기동물</div>\r\n"
-    testZoo += "</body> \r\n"
-    testZoo += "</html> \r\n"
+    var farmDetail = ""
+    farmDetail += "{ \r\n"
+    farmDetail += "\"가게이름\" : \"윤이나 농장\", \r\n"
+    farmDetail += "\"파는 물건\" : \"농산물, 해산물\", \r\n"
+    farmDetail += "\"배송여부\" : true \r\n"
+    farmDetail += "}"
 
 
-
-
-
-    //file함수의 메소드와 유사
-    File("test").mkdirs()
-
-
-    File("test/testZoo.html").writeText(testZoo)
+    File("farmRepository/farm/introduce/data/detail/farmDetail.json").parentFile.mkdirs()
+    File("farmRepository/farm/introduce/data/detail/farmDetail.json").writeText(farmDetail)
 
 }
-
-fun zooJson() {
-
-    var testZoo = ""
-
-    testZoo += "{\r\n"
-    testZoo += "\"우리 동물원 1번\": \"사파리\", \r\n"
-    testZoo += "\t \"사파리 동물\": { \r\n"
-    testZoo +=  "\t\t \"육식동물 1\" : \"사자\",\r\n"
-    testZoo += "\t\t \"육식동물 2\" : \"호랑이\",\r\n"
-    testZoo += "\t\t \"초식동물 1\" : \"기린\",\r\n"
-    testZoo += "\t\t \"초식동물 2\" : \"코끼리\"\r\n"
-    testZoo +=  "}, \r\n"
-    testZoo += "\"우리 동물원 2번\": \"아쿠아리움\", \r\n"
-    testZoo += "\t \"아쿠아리움 물고기\": {\r\n"
-    testZoo += "\t\t \"열대어 1\" : \"구피\",\r\n"
-    testZoo += "\t\t \"열대어 2\" : \"플래티\",\r\n"
-    testZoo += "\t\t \"해수어 1\" : \"상어\",\r\n"
-    testZoo += "\t\t \"해수어 2\" : \"돌고래\"\r\n"
-    testZoo += "}, \r\n"
-    testZoo += "\"우리 동물원 3번\": \"아기동물\", \r\n"
-    testZoo += "\t \"사파이 아기동물\": {\r\n"
-    testZoo += "\t\t \"사자\" : \"3개월\",\r\n"
-    testZoo += "\t\t \"호랑이\" : \"8개월\"\r\n"
-    testZoo += "}, \r\n"
-    testZoo += "\"직원\" : [\"짱구\", \"철수\", \"맹구\", \"유리\", \"훈이\"], \r\n"
-    testZoo += "\"월급\" : true, \r\n"
-    testZoo += "\"연봉\" : 3000, \r\n"
-    testZoo += "\"연봉 상승률\" : 2.5, \r\n"
-    testZoo += "\"월 광관객  수\" : \"50000명\" \r\n"
-    testZoo += "}"
-
-
-    File("test/JSONDATA/testZoo2.json").parentFile.mkdirs()
-
-    File("test/JSONDATA/testZoo2.json").writeText(testZoo)
-
-
-}
-
